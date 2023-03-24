9f328df4191939509891537d4d0cb585bd640c59
bryanmauv <bmauvoisin12@gmail.com>
2023-03-24 15:20:25 +0100

    ex1

--- exerciceDHCP1.ps1
+++ exerciceDHCP1.ps1
@@ -6,6 +6,22 @@ $premiereAdresse = Read-Host "Entrez la première adresse à distribuer"
 $derniereAdresse = Read-Host "Entrez la dernière adresse à distribuer"
 $adressePasserelle = Read-Host "Entrez l'adresse de passerelle à diffuser"
 
+# Afficher les informations de l'étendue DHCP à créer
+Write-Host "Vous êtes sur le point de créer l'étendue DHCP suivante :"
+Write-Host "Nom : $nomEtendue"
+Write-Host "Adresse réseau : $adresseReseau"
+Write-Host "Masque sous-réseau : $masqueSousReseau"
+Write-Host "Première adresse à distribuer : $premiereAdresse"
+Write-Host "Dernière adresse à distribuer : $derniereAdresse"
+Write-Host "Adresse de passerelle : $adressePasserelle"
+
+# Demander à l'utilisateur de confirmer la création de l'étendue DHCP
+$confirmation = Read-Host "Voulez-vous vraiment créer cette étendue DHCP ? (Oui/Non)"
+if ($confirmation -ne "Oui") {
+    Write-Host "Opération annulée."
+    exit
+}
+
 # Créer l'étendue DHCP en utilisant les informations fournies par l'utilisateur
 Add-DhcpServerv4Scope -Name $nomEtendue -StartRange $premiereAdresse -EndRange $derniereAdresse -SubnetMask $masqueSousReseau -Description "Etendue DHCP créée par script PowerShell" -LeaseDuration 8.00:00:00 -State Active
 
